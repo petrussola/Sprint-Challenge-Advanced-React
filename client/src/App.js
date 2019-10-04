f7c57bda36d97153f9e2f597a0d971870cef1bde
Pere <pere.solaclaver@gmail.com>
2019-10-04 08:46:20 +0100

    Fetch data from server with axios and set it to state

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
 
+import axios from "axios";
+
+const playersApi = "http://localhost:5000/api/players";
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: []
     };
+  }
+
+  componentDidMount() {
+    axios
+      .get(playersApi)
+      .then(res => {
+        this.setState(res.data)
+      })
+      .catch(error => {
+        debugger;
+      });
   }
 
   render() {
